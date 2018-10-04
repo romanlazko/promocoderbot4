bd6145b143cfb7a5e2da2f8d3f8de32bd14de418
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-04 18:37:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,9 +17,9 @@ if ($text == "/start") {
         'chat_id' => $chat_id,
         'text' => $reply,    
     ];
-  sendMessage($parameters);
+  sendMessage($token,$parameters);
 }
-function sendMessage($parameters){
+function sendMessage($token,$parameters){
     
     $url = 'https://api.telegram.org/bot' . $token . '/sendMessage?' . http_build_query($parameters);
     file_get_contents($url);
