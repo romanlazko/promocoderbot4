eac0206bd408a7fcb6f841cd487d70f3ddba3484
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-04 20:03:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,19 +13,23 @@ $chat_id = $output['message']['chat']['id'];
 
 if ($text == "/start") {
     $reply = "Добро пожаловать в бота!";
-    sendMessage($token,$chat_id,$reply,sendKeyboard());
+    sendKeyboard($token,$chat_id);
+    sendMessage($token,$chat_id,$reply);
 }
-function sendMessage($token,$chat_id,$reply, $key){
+function sendMessage($token,$chat_id,$reply){
     $parameters = [
         'chat_id' => $chat_id,
         'text' => $reply,   
-        'reply_markup' => $key,
     ];
     $url = 'https://api.telegram.org/bot' . $token . '/sendMessage?' . http_build_query($parameters);
     file_get_contents($url);
 }
-function sendKeyboard(){
+function sendKeyboard($token,$chat_id){
     $buttons = [["Последние статьи"],["Картинка"],["Гифка"]]; //Клавиатура
-    $reply_markup =  [ 'keyboard' => $buttons, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);    
-    return $reply_markup;
+    $reply_markup =  ([ 'keyboard' => $buttons, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);    
+    $parameters = [
+        'chat_id' => $chat_id,
+    ];
+    $url = 'https://api.telegram.org/bot' . $token . '/sendMessage?' . http_build_query($parameters) . '&reply_markup=' . $reply_markup;
+    file_get_contents($url);
 }
