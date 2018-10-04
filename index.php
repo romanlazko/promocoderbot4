ce6026d7b2b6972ea18524ec0bc3452f391f53df
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-04 20:18:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,14 @@ $output = json_decode(file_get_contents('php://input'),true);
 $text = $output['message']['text'];
 $chat_id = $output['message']['chat']['id'];
 
-if ($text == "/start") {
-    $reply = "Добро пожаловать в бота!";
-    sendKeyboard($token,$chat_id);
+$buttons = [["Последние статьи"],["Картинка"],["Гифка"]];
+sendKeyboard($token,$chat_id,$buttons);
+
+if ($text == "Последние статьи") {
+    $reply = "Вы выбрали 'Последние статьи'";
     sendMessage($token,$chat_id,$reply);
 }
+
 function sendMessage($token,$chat_id,$reply){
     $parameters = [
         'chat_id' => $chat_id,
@@ -24,15 +27,12 @@ function sendMessage($token,$chat_id,$reply){
     $url = 'https://api.telegram.org/bot' . $token . '/sendMessage?' . http_build_query($parameters);
     file_get_contents($url);
 }
-function sendKeyboard($token,$chat_id){
-    $buttons = [["Последние статьи"],["Картинка"],["Гифка"]]; //Клавиатура
-    $keyboard =  json_encode($keyboard = [ 'keyboard' => $buttons, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);  
-    
+function sendKeyboard($token,$chat_id,$buttons){
+    $keyboard =  json_encode($keyboard = [ 'keyboard' => $buttons, 'resize_keyboard' => true, 'one_time_keyboard' => true ]);  
     $parameters = [
         'chat_id' => $chat_id,
         'text' => "Клавиатура", 
         'reply_markup' => $keyboard,
     ];
     file_get_contents('https://api.telegram.org/bot' . $token . '/sendMessage?' . http_build_query($parameters));
-    
 }
