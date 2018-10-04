c2a7c58a8bc64bdbe33307762fb6bc91b6bafd49
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-04 17:15:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Date: 04-Oct-18
  * Time: 15:51
  */
-$output = file_get_contents('php://input');
-
-file_put_contents('logs.txt', $output);
+$output = ison_decode(file_get_contents('php://input'),true);
+$text = $output['message']['text'];
+file_put_contents('logs.txt', $text);
 
