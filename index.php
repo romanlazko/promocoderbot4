a74321a23dd5285b440cf8f52accbeab32be421b
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-05 13:55:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,26 +11,37 @@ $output = json_decode(file_get_contents('php://input'),true);
 $text = $output['message']['text'];
 $chat_id = $output['message']['chat']['id'];
 
-if ($text == "/start") {
+if ($text == "/start" ) {
     $reply = "Добро пожаловать в бота!";
     $buttons = [["Еда и напитки"],["Развлечения и досуг"],["Доставка"]];
-    sendKeyboard($token,$chat_id,$buttons);
-    sendMessage($token,$chat_id,$reply);
+    /*sendKeyboard($token,$chat_id,$buttons);*/
+    sendMessage($token,$chat_id,$reply,$buttons);
 }
+if ($text == "Главное меню") {
+    $reply = "";
+    $buttons = [["Еда и напитки"],["Развлечения и досуг"],["Доставка"]];
+    /*sendKeyboard($token,$chat_id,$buttons);*/
+    sendMessage($token,$chat_id,$reply,$buttons);
+}
+
 if ($text == "Еда и напитки") {
     $reply = "Вы выбрали 'Еда и напитки'";
-    $buttons = [["Кафе"],["Кофе"],["Ресторан"]];
-    sendKeyboard($token,$chat_id,$buttons);
-    sendMessage($token,$chat_id,$reply);
+    $buttons = [["Кафе"],["Кофе"],["Ресторан"],["Главное меню"]];
+    /*sendKeyboard($token,$chat_id,$buttons);*/
+    sendMessage($token,$chat_id,$reply,$buttons);
 }
 if ($text == "Развлечения и досуг") {
     $reply = "Вы выбрали 'Развлечения и досуг'";
-    sendMessage($token,$chat_id,$reply);
+    $buttons = [["Кафе"],["Кофе"],["Ресторан"],["Главное меню"]];
+    /*sendKeyboard($token,$chat_id,$buttons);*/
+    sendMessage($token,$chat_id,$reply,$buttons);
 }
-function sendMessage($token,$chat_id,$reply){
+function sendMessage($token,$chat_id,$reply,$buttons){
+    $keyboard =  json_encode($keyboard = [ 'keyboard' => $buttons, 'resize_keyboard' => false, 'one_time_keyboard' => false ]);
     $parameters = [
         'chat_id' => $chat_id,
-        'text' => $reply,   
+        'text' => $reply,  
+        'reply_markup' => $keyboard,
     ];
     $url = 'https://api.telegram.org/bot' . $token . '/sendMessage?' . http_build_query($parameters);
     file_get_contents($url);
