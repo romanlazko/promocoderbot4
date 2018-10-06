064d29fcf3167f3ef573cc50ce91926d44a307ad
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-06 14:24:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,40 +10,41 @@ $chat_id = $output['message']['chat']['id'];
 if ($text == "/start" ) {
     $reply = "Добро пожаловать в бота!";
     $buttons = [["Еда и напитки"],["Развлечения и досуг"],["Доставка"]];
-    /*sendKeyboard($token,$chat_id,$buttons);*/
-    sendMessage($token,$chat_id,$reply,$buttons);
+    sendKeyboard($token,$chat_id,$buttons);
+    sendMessage($token,$chat_id,$reply);
 }
 if ($text == "Главное меню") {
     $reply = "Главное меню";
-    /*$buttons = [["Еда и напитки"],["Развлечения и досуг"],["Доставка"]];*/
-    /*sendKeyboard($token,$chat_id,$buttons);*/
-    sendMessage($token,$chat_id,$reply,$buttons);
+    $buttons = [["Еда и напитки"],["Развлечения и досуг"],["Доставка"]];
+    sendKeyboard($token,$chat_id,$buttons);
+    sendMessage($token,$chat_id,$reply);
 }
 
 if ($text == "Еда и напитки") {
     $reply = "Вы выбрали 'Еда и напитки'";
     $buttons = [["Кафе"],["Кофе"],["Ресторан"],["Главное меню"]];
-    /*sendKeyboard($token,$chat_id,$buttons);*/
-    sendMessage($token,$chat_id,$reply,$buttons);
+    sendKeyboard($token,$chat_id,$buttons);
+    sendMessage($token,$chat_id,$reply);
 }
 if ($text == "Развлечения и досуг") {
     $reply = "Вы выбрали 'Развлечения и досуг'";
     $buttons = [["Кафе"],["Кофе"],["Ресторан"],["Главное меню"]];
-    /*sendKeyboard($token,$chat_id,$buttons);*/
-    sendMessage($token,$chat_id,$reply,$buttons);
+    sendKeyboard($token,$chat_id,$buttons);
+    sendMessage($token,$chat_id,$reply);
 }
-function sendMessage($token,$chat_id,$reply,$buttons){
-    $keyboard =  json_encode($keyboard = [ 'keyboard' => $buttons, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+function sendMessage($token,$chat_id,$reply){
     $parameters = [
         'chat_id' => $chat_id,
         'text' => $reply,  
-        'reply_markup' => $keyboard,
     ];
     $url = 'https://api.telegram.org/bot' . $token . '/sendMessage?' . http_build_query($parameters);
     file_get_contents($url);
 }
 function sendKeyboard($token,$chat_id,$buttons){
-    $keyboard =  json_encode($keyboard = [ 'keyboard' => $buttons, 'resize_keyboard' => false, 'one_time_keyboard' => false ]);  
+    $keyboard =  json_encode($keyboard = ['keyboard' => $buttons, 
+                                          'resize_keyboard' => false, 
+                                          'one_time_keyboard' => false , 
+                                          'selective' => false]);  
     $parameters = [
         'chat_id' => $chat_id, 
         'text' => 'Клавиатура', 
