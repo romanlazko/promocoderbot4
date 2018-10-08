9915c7521b54e39817f1a12af2953296358bc4bc
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-08 21:48:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
 <?php
-$login = "roman";
 
-$servername="db4free.net: 3306";
-    $username="promocoder";
-    $password="zdraste1234";
-    $dbname="promocoder";
-    $dbconnect = new mysqli($servername, $username, $password, $dbname); 
- 
-    
-            
-            $ucertable = "CREATE TABLE $login (
-                login VARCHAR(30) NOT NULL,
-                test1result VARCHAR(30) NOT NULL,
-                test2result VARCHAR(30) NOT NULL)";
-            
-            if($dbconnect->query($ucertable) === TRUE ){
-                
-                echo "Регистрация прошла успешно"."<br>";
-                
-            }
     
 $token = "633839981:AAFtfuE_KVcHt1huA9RV6txQczt9It3xzI0";
 $output = json_decode(file_get_contents('php://input'),true);
