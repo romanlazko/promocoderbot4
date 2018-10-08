16c54aba8500ac536ef012c4aac7e1eeeb3f72b6
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-09 01:55:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -74,12 +74,12 @@ if(isset($output['callback_query']['data'])){
     $message_id = $output['callback_query']['message']['message_id'];
     $message = $output['callback_query']['message']['text'];
     sendMessage($token,$inline_chat_id,$message_id);
-    editMassage($token,$chat_id);
+    editMassage($token,$chat_id, '633');
 }
-function editMassage($token,$chat_id){
+function editMassage($token,$chat_id,$message_id){
     $parameters = [
         'chat_id' => $chat_id, 
-        'message_id' => '628', 
+        'message_id' => $message_id, 
         'text' => 'Вы нажали на кнопку',
     ];
     file_get_contents('https://api.telegram.org/bot' . $token . '/editMessageText?' . http_build_query($parameters));
