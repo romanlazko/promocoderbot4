033b7d42fd145e44469cb34f65584799f84a9905
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-09 01:57:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if ($text == "Инлайн Клавиатура") {
 }
 if ($text == "EDIT") {
     $reply = "Удалено";
-    editMassage($token,$chat_id);
+    editMassage($token,$chat_id, '633');
     sendMessage($token,$chat_id,$reply);
 }
 function sendMessage($token,$chat_id,$reply){
