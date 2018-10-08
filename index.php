5a9d5d28090f973dafc556136c1244365736c070
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-09 01:28:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ function inlineKeyboard($token,$chat_id,$reply){
 }
 if(isset($output['callback_query']['data'])){
     $inline_chat_id = $output['callback_query']['message']['chat']['id'];
-    $massage_id = $output['callback_query']['id'];
+    $massage_id = $output['callback_query']['message']['id'];
     $message = $output['callback_query']['message']['text'];
     sendMessage($token,$inline_chat_id,$massage_id);
     sendMessage($token,$inline_chat_id,$message);
