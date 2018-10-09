4ed2d4c85808327dcc270cb807303c43693fdaf1
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-09 15:46:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ if(isset($output['callback_query']['data'])){
     $inline_chat_id = $output['callback_query']['message']['chat']['id'];
     $message_id = ['callback_query']['message']['id'];
     sendMessage($token,$inline_chat_id,$inline_data);
-    sendMessage($token,$inline_chat_id,$message_id);
+    sendMessage($token,$inline_chat_id,$inline_chat_id);
 }
 function editMassage($token,$chat_id,$message_id){
     $parameters = [
