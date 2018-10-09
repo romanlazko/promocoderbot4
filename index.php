20d521f3bcf9ec109d40cc03566e438dc4de996e
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-09 16:35:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ if ($text == "Инлайн Клавиатура") {
 if ($text == "EDIT") {
     $reply = "ИЗМЕНЕНО".$message_id;
     editMassage($token,$chat_id,$message_id);
-    sendMessage($token,$chat_id,$reply);
+    sendMessage($token,$chat_id,$message_id);
 }
 
 function sendMessage($token,$chat_id,$reply){
