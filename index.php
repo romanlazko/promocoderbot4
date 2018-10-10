543e491ea4335042e9d890d1a27e08762662df89
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-10 16:51:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,44 @@ $message_id839 = '865';
 $latitude=$output['message']['location']['latitude'];
 $longitude = $output['message']['location']['longitude'];
 
+define('EARTH_RADIUS', 6372795);
+ 
+/*
+ * Расстояние между двумя точками
+ * $φA, $λA - широта, долгота 1-й точки,
+ * $φB, $λB - широта, долгота 2-й точки
+ * Написано по мотивам http://gis-lab.info/qa/great-circles.html
+ * Михаил Кобзарев <mikhail@kobzarev.com>
+ *
+ */
+function distance($φA, $λA, $φB, $λB) {
+ 
+    // перевести координаты в радианы
+    $lat1 = $φA * M_PI / 180;
+    $lat2 = $φB * M_PI / 180;
+    $long1 = $λA * M_PI / 180;
+    $long2 = $λB * M_PI / 180;
+ 
+    // косинусы и синусы широт и разницы долгот
+    $cl1 = cos($lat1);
+    $cl2 = cos($lat2);
+    $sl1 = sin($lat1);
+    $sl2 = sin($lat2);
+    $delta = $long2 - $long1;
+    $cdelta = cos($delta);
+    $sdelta = sin($delta);
+ 
+    // вычисления длины большого круга
+    $y = sqrt(pow($cl2 * $sdelta, 2) + pow($cl1 * $sl2 - $sl1 * $cl2 * $cdelta, 2));
+    $x = $sl1 * $sl2 + $cl1 * $cl2 * $cdelta;
+ 
+    //
+    $ad = atan2($y, $x);
+    $dist = $ad * EARTH_RADIUS;
+ 
+    return $dist;
+}
+
 if(isset($inline_data)){
     if($inline_data == 'but1'){
         $message = 'ВЫ ВЫБРАЛИ ПЕРВУЮ КНОПКУ';
@@ -25,7 +63,7 @@ if(isset($inline_data)){
 if(isset($latitude) or isset($longitude)){
     sendMessage($token,$chat_id,$longitude);
     sendMessage($token,$chat_id,$latitude);
-
+    sendMessage($token,$chat_id,distance('48.4420860','35.0160808',$latitude,$longitude));
 }
 
 if ($text == "/start" ) {
