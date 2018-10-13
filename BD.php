af94ea4e28731864b3bfa95fe26bd789c9d2eb66
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-13 16:53:19 +0300

    Update BD.php

--- BD.php
+++ BD.php
@@ -10,22 +10,19 @@ function userfunc($user_id){
 
     $bd_user_id = "SELECT user_id FROM users";
     $result = $dbconnect->query($bd_user_id);
-    if($result->num_rows>0){
-        while($row = $result->fetch_assoc()){
-            if($row['user_id']==$user_id){
-                sendMessage($token,$chat_id,'ТЫ УЖЕ ОПЫТНЫЙ ПОЛЬЗОВАТЕЛЬ');
-                break;
-            }
-            else{
-                $createUser = "INSERT INTO users(user_id) VALUES('$user_id')";
-                if($dbconnect->query($createUser) === TRUE ){
-                     sendMessage($token,$chat_id,'ТЫ НОВЫЙ ПОЛЬЗОВАТЕЛЬ');
-
-                }               
-            }
+    
+    while($row = $result->fetch_assoc()){
+        if($row['user_id']==$user_id){
+            sendMessage($token,$chat_id,'ТЫ УЖЕ ОПЫТНЫЙ ПОЛЬЗОВАТЕЛЬ');
+            break;
         }
-        
-    }
+        else{
+            $createUser = "INSERT INTO users(user_id) VALUES('$user_id')";
+            if($dbconnect->query($createUser) === TRUE ){
+                 sendMessage($token,$chat_id,'ТЫ НОВЫЙ ПОЛЬЗОВАТЕЛЬ');
+            }               
+        }
+    }   
     $dbconnect->close(); 
 }
 
