6b0fbc7704d8c6a197d6b2ee0abd2450db98a569
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-13 17:36:13 +0300

    Update BD.php

--- BD.php
+++ BD.php
@@ -2,16 +2,16 @@
 
 
 function userfunc($token,$chat_id,$user_id){
-    sendMessage($token,$chat_id,'нихуя'); 
-    /*$servername="db4free.net: 3306";
+    sendMessage($token,$chat_id,$user_id); 
+    $servername="db4free.net: 3306";
     $username="promocoder";
     $password="zdraste1234";
     $dbname="promocoder";
     $dbconnect = new mysqli($servername, $username, $password, $dbname); 
-    $bool = FALSE;
+    
     $createUser = "INSERT INTO users(user_id) VALUES('$user_id')";
     $createUser = "INSERT INTO users(user_id) VALUES('$chat_id')";
-    $sql = "SELECT user_id FROM users";
+    /*$sql = "SELECT user_id FROM users";
     $result = $dbconnect->query($sql);
     
         while($row = $result->fetch_assoc()){
@@ -24,8 +24,8 @@ function userfunc($token,$chat_id,$user_id){
                 sendMessage($token,$chat_id,$result); 
                 $bool = FALSE;
             }
-        }   
-    $dbconnect->close(); */
+        }   */
+    $dbconnect->close(); 
 }
 
 ?>
