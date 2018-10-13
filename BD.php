36016134dacd1818243803ed3d4bb75e45df82e9
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-13 18:55:17 +0300

    Update BD.php

--- BD.php
+++ BD.php
@@ -1,30 +1,26 @@
 <?php
-
+$servername="db4free.net: 3306";
+$username="promocoder";
+$password="zdraste1234";
+$dbname="promocoder";
+$dbconnect = new mysqli($servername, $username, $password, $dbname); 
 
 function userfunc($token,$chat_id,$user_id){
-    
-    $servername="db4free.net: 3306";
-    $username="promocoder";
-    $password="zdraste1234";
-    $dbname="promocoder";
-    $dbconnect = new mysqli($servername, $username, $password, $dbname); 
-    
-    
+   
     $sql = "SELECT user_id FROM users";
     $result = $dbconnect->query($sql);
-        while($row = $result->fetch_assoc()){
-            if($row['user_id']==$user_id){
-                sendMessage($token,$chat_id,'ТЫ СТАРЫЙ ПОЛЬЗОВАТЕЛЬ');
-                break;
-            }
-            else{
-                $createUser = "INSERT INTO users(user_id) VALUES('$user_id')";
-                if($dbconnect->query($createUser) === TRUE){
+    while($row = $result->fetch_assoc()){
+        if($row['user_id']==$user_id){
+            sendMessage($token,$chat_id,'ТЫ СТАРЫЙ ПОЛЬЗОВАТЕЛЬ');
+            break;
+        }
+        else{
+            $createUser = "INSERT INTO users(user_id) VALUES('$user_id')";
+            if($dbconnect->query($createUser) === TRUE){
                 sendMessage($token,$chat_id,'ТЫ НОВЫЙ ПОЛЬЗОВАТЕЛЬ'); 
-                }
             }
-        }   
-    $dbconnect->close(); 
+        }
+    }   
 }
-
+$dbconnect->close();
 ?>
