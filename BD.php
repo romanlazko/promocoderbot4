d516c079fae6557c650f737f3156a975b4aa6f30
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-13 19:04:31 +0300

    Update BD.php

--- BD.php
+++ BD.php
@@ -6,6 +6,10 @@ $dbname="promocoder";
 
 
 function userfunc($token,$chat_id,$user_id){
+   $servername="db4free.net: 3306";
+$username="promocoder";
+$password="zdraste1234";
+$dbname="promocoder";
    $dbconnect = new mysqli($servername, $username, $password, $dbname); 
     $sql = "SELECT user_id FROM users";
     $result = $dbconnect->query($sql);
