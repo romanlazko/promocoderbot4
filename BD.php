dbd15d1121ef9192057c9265057100e6889d8945
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-13 17:17:49 +0300

    Update BD.php

--- BD.php
+++ BD.php
@@ -9,18 +9,19 @@ function userfunc($user_id){
     $dbconnect = new mysqli($servername, $username, $password, $dbname); 
     
     $bool = FALSE;
-    
+    $createUser = "INSERT INTO users(user_id) VALUES('$user_id')";
+    $createUser = "INSERT INTO users(user_id) VALUES('$chat_id')";
     $sql = "SELECT user_id FROM users";
     $result = $dbconnect->query($sql);
     
         while($row = $result->fetch_assoc()){
             if($row['user_id']==$user_id){
-                sendMessage($token,$chat_id,$row['user_id']); 
+                sendMessage($token,$chat_id,$result); 
                 $bool=TRUE;                
                 break;
             }
             else{
-                sendMessage($token,$chat_id,$row['user_id']); 
+                sendMessage($token,$chat_id,$result); 
                 $bool = FALSE;
             }
         }    
@@ -29,7 +30,7 @@ function userfunc($user_id){
         
                   
     if($bool===FALSE){
-        $createUser = "INSERT INTO users(user_id) VALUES('$user_id')";
+        
         sendMessage($token,$chat_id,'ТЫ НОВЫЙ ПОЛЬЗОВАТЕЛЬ'); 
     }
     else{  
