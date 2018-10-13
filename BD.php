66a6d771d9ae16242eba8c9eda0b8eb4f58cab6e
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-13 18:08:05 +0300

    Update BD.php

--- BD.php
+++ BD.php
@@ -9,20 +9,19 @@ function userfunc($token,$chat_id,$user_id){
     $dbname="promocoder";
     $dbconnect = new mysqli($servername, $username, $password, $dbname); 
     
-    /*$createUser = "INSERT INTO users(user_id) VALUES('$user_id')";
-    if($dbconnect->query($createUser) === TRUE){
-        
-    }*/
+    
     $sql = "SELECT user_id FROM users";
     $result = $dbconnect->query($sql);
         while($row = $result->fetch_assoc()){
             if($row['user_id']==$user_id){
-                sendMessage($token,$chat_id,'work');           
+                sendMessage($token,$chat_id,'ТЫ СТАРЫЙ ПОЛЬЗОВАТЕЛЬ');
                 break;
             }
             else{
-                sendMessage($token,$chat_id,'work2'); 
-                
+                $createUser = "INSERT INTO users(user_id) VALUES('$user_id')";
+                if($dbconnect->query($createUser) === TRUE){
+                sendMessage($token,$chat_id,'ТЫ НОВЫЙ ПОЛЬЗОВАТЕЛЬ'); 
+                }
             }
         }   
     $dbconnect->close(); 
