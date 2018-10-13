569b8878709dcfed15dd6988d87382dda381de29
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-13 16:41:51 +0300

    Update BD.php

--- BD.php
+++ BD.php
@@ -1,14 +1,15 @@
 <?php
-$servername="db4free.net: 3306";
-$username="promocoder";
-$password="zdraste1234";
-$dbname="promocoder";
-$dbconnect = new mysqli($servername, $username, $password, $dbname); 
 
 
 function userfunc($user_id){
+    $servername="db4free.net: 3306";
+    $username="promocoder";
+    $password="zdraste1234";
+    $dbname="promocoder";
+    $dbconnect = new mysqli($servername, $username, $password, $dbname); 
+
     $bd_user_id = "SELECT user_id FROM users";
-    $result = $dbconnect ->query($bd_user_id);
+    $result = $dbconnect->query($bd_user_id);
     if($result->num_rows>0){
         while($row = $result->fetch_assoc()){
             if($row['user_id']==$user_id){
