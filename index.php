cb476b296e9e26307883bf0c22e59a1406844323
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-13 21:21:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $user_id = $output['message']['from']['id'];
 
 include 'distance.php';
 include 'BD.php';
-create($token,$chat_id);
+create($token,$chat_id,$dbconnect);
 if(isset($inline_data)){
     
     if($inline_data == 'eatAndFood'){
