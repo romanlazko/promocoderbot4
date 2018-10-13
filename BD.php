0c02235dc501ef4d1f9ca7cafbe27a965ac891e2
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-13 15:42:17 +0300

    Update and rename bd.php to BD.php

---
+++ BD.php
@@ -0,0 +1,36 @@
+<?php
+
+$servername="db4free.net: 3306";
+$username="promocoder";
+$password="zdraste1234";
+$dbname="promocoder";
+$dbconnect = new mysqli($servername, $username, $password, $dbname); 
+
+function user($user_id){
+    $bd_user_id = "SELECT user_id FROM users";
+    $result = $dbconnect ->query($bd_user_id);
+    if($result->num_rows>0){
+        while($row = $result->fetch_assoc()){
+            if($row['user_id']==$user_id){
+                sendMessage($token,$chat_id,'ТЫ УЖЕ ОПЫТНЫЙ ПОЛЬЗОВАТЕЛЬ');
+                break;
+            }
+            else{
+                $createUser = "INSERT INTO users(user_id) VALUES($user_id)";
+            }
+        }
+        
+    }
+    else{
+                $createUser = "INSERT INTO users(user_id) VALUES($user_id)";
+            }
+/*$ucertable = "CREATE TABLE $login (
+    login VARCHAR(30) NOT NULL,
+    test1result VARCHAR(30) NOT NULL,
+    test2result VARCHAR(30) NOT NULL)";      
+    if($dbconnect->query($ucertable) === TRUE ){
+        echo "Регистрация прошла успешно"."<br>";
+                
+    }*/
+}
+?>
