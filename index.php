3cfba1bbe9c4b6af4310591c262d34444cf40cd8
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-13 16:11:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
-    
+$login = "users";
+$servername="db4free.net: 3306";
+$username="promocoder";
+$password="zdraste1234";
+$dbname="promocoder";
+$dbconnect = new mysqli($servername, $username, $password, $dbname); 
+
+$ucertable = "CREATE TABLE $login (
+    login VARCHAR(30) NOT NULL,
+    test1result VARCHAR(30) NOT NULL,
+    test2result VARCHAR(30) NOT NULL)";      
+    if($dbconnect->query($ucertable) === TRUE ){
+        sendMessage($token,$chat_id,'получилось');
+                
+    }
 $token = "633839981:AAFtfuE_KVcHt1huA9RV6txQczt9It3xzI0";
 $output = json_decode(file_get_contents('php://input'),true);
 
@@ -11,7 +25,7 @@ $chat_id = $output['message']['chat']['id'];
 $user_id = $output['message']['from']['id'];
 
 include 'distance.php';
-include 'BD.php';
+//include 'BD.php';
 
 if(isset($inline_data)){
     
