79c08116e987725a8732b917512c642237827d4c
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-13 16:26:53 +0300

    Update BD.php

--- BD.php
+++ BD.php
@@ -1,19 +1,12 @@
 <?php
-$slovo = "slovo";
 $servername="db4free.net: 3306";
 $username="promocoder";
 $password="zdraste1234";
 $dbname="promocoder";
 $dbconnect = new mysqli($servername, $username, $password, $dbname); 
 
-$createUser = "INSERT INTO users(user_id) VALUES('$slovo')";
-if($dbconnect->query($createUser) === TRUE ){
-        sendMessage($token,$chat_id,'получилось');
-                
-    }
-        
 
-/*function userfunc($user_id){
+function userfunc($user_id){
     $bd_user_id = "SELECT user_id FROM users";
     $result = $dbconnect ->query($bd_user_id);
     if($result->num_rows>0){
@@ -24,24 +17,15 @@ if($dbconnect->query($createUser) === TRUE ){
             }
             else{
                 $createUser = "INSERT INTO users(user_id) VALUES($user_id)";
-                sendMessage($token,$chat_id,'ТЫ НОВЫЙ ПОЛЬЗОВАТЕЛЬ');
-                
+                if($dbconnect->query($createUser) === TRUE ){
+                     sendMessage($token,$chat_id,'ТЫ НОВЫЙ ПОЛЬЗОВАТЕЛЬ');
+
+                }               
             }
         }
         
     }
-    else{
-                $createUser = "INSERT INTO users(user_id) VALUES($user_id)";
-        sendMessage($token,$chat_id,'ТЫ НОВЫЙ ПОЛЬЗОВАТЕЛЬ');
-                
-            }
-$ucertable = "CREATE TABLE $login (
-    login VARCHAR(30) NOT NULL,
-    test1result VARCHAR(30) NOT NULL,
-    test2result VARCHAR(30) NOT NULL)";      
-    if($dbconnect->query($ucertable) === TRUE ){
-        echo "Регистрация прошла успешно"."<br>";
-                
-    }*/
+    
 }
+$dbconnect->close(); 
 ?>
