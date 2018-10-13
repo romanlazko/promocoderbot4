8d3dae8e3df7b4fcfe0141074d4aa0978b44c938
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-13 17:07:04 +0300

    Update BD.php

--- BD.php
+++ BD.php
@@ -7,21 +7,32 @@ function userfunc($user_id){
     $password="zdraste1234";
     $dbname="promocoder";
     $dbconnect = new mysqli($servername, $username, $password, $dbname); 
-
-    $bd_user_id = "SELECT user_id FROM users";
-    $result = $dbconnect->query($bd_user_id);
     
-    /*while($row = $result->fetch_assoc()){
-        if($row['user_id']==$user_id){
-            sendMessage($token,$chat_id,'ТЫ УЖЕ ОПЫТНЫЙ ПОЛЬЗОВАТЕЛЬ');
-            break;
-        }
-        else{*/
-    $createUser = "INSERT INTO users(user_id) VALUES('$user_id')";
-    if($dbconnect->query($createUser) === TRUE ){
-         sendMessage($token,$chat_id,'ТЫ НОВЫЙ ПОЛЬЗОВАТЕЛЬ');
-    }               
+    $bool = FALSE;
+    
+    $sql = "SELECT user_id FROM users";
+    $result = $dbconnect->query($sql);
+    if($result->num_rows>0){
+        while($row = $result->fetch_assoc()){
+            if($row['user_id']==$login){
+                $bool=TRUE;                
+                break;
+            }
+            else{
+                $bool = FALSE;
+            }
+        }    
+    }
+    
         
+                  
+    if($bool===FALSE){
+        $createUser = "INSERT INTO users(user_id) VALUES('$user_id')";
+        sendMessage($token,$chat_id,'ТЫ НОВЫЙ ПОЛЬЗОВАТЕЛЬ'); 
+    }
+    else{  
+        sendMessage($token,$chat_id,'ТЫ старый ПОЛЬЗОВАТЕЛЬ'); 
+    }
       
     $dbconnect->close(); 
 }
