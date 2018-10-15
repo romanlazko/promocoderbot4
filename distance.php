951c2557abfce84b0ae223aeac77a0fa164b3ea6
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-15 23:28:15 +0300

    Update distance.php

--- distance.php
+++ distance.php
@@ -1,5 +1,5 @@
 <?php
-include 'BD.php';
+//include 'BD.php';
 
 $latitude = $output['message']['location']['latitude'];
 $longitude = $output['message']['location']['longitude'];
@@ -31,7 +31,7 @@ function distance($φA, $λA, $φB, $λB) {
 $dist = distance('48.4420860','35.0160808',$latitude,$longitude);
 
 if(isset($latitude) or isset($longitude)){
-    update($token,$chat_id,$dbconnect);
+    //update($token,$chat_id,$dbconnect);
     if($dist < 20000){
         $reply = 'Ваш город Днепр'.$dist;
         $buttons = [["Настройки"],["Категории"]];
