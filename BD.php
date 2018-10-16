7f4c04cd46e2c3e05d090e4aad73e10dcf19bcc5
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-16 23:20:29 +0300

    Update BD.php

--- BD.php
+++ BD.php
@@ -5,12 +5,12 @@ function userfunc($token,$chat_id,$user_id,$dbconnect){
     
     $result = $dbconnect->query("SELECT user_id FROM users");
     while($row = $result->fetch_assoc()){
-        if($row['user_id']==$user_id){
+        if($row['user_id']==$chat_id){
             sendMessage($token,$chat_id,'ТЫ СТАРЫЙ ПОЛЬЗОВАТЕЛЬ');
             break;
         }
         else{
-            $createUser = "INSERT INTO users(user_id) VALUES('$user_id')";
+            $createUser = "INSERT INTO users(user_id) VALUES('$chat_id')";
             
             if($dbconnect->query($createUser) === TRUE){
                 sendMessage($token,$chat_id,'ТЫ НОВЫЙ ПОЛЬЗОВАТЕЛЬ'); 
