97ac59d9533b19b52257161ee3e80b04b12a00e2
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-16 21:34:49 +0300

    Update BD.php

--- BD.php
+++ BD.php
@@ -27,8 +27,15 @@ function update($token,$chat_id,$dbconnect,$user_id,$latitude,$longitude){
 }
 function takePos($dbconnect,$inline_data){
     $result = $dbconnect->query("SELECT posName FROM EatAndDrinks WHERE posShow = '1'");
-    for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row)
-    sendMessage($token,$chat_id,$data);
+    while($row = $result->fetch_assoc()){
+        sendMessage($token,$chat_id,$row['user_id']);
+        
+//         if($row['user_id']==$user_id){
+//             sendMessage($token,$chat_id,'ТЫ СТАРЫЙ ПОЛЬЗОВАТЕЛЬ');
+//         }
+    } 
+//     for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row)
+//     sendMessage($token,$chat_id,$data);
 //     if($dbconnect->query($result) === TRUE){
 //         sendMessage($token,$chat_id,$result); 
 //     }
