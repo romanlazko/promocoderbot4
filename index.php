7391218f9299d6255dd1a5c51575ad08bd93201f
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-17 12:24:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if(isset($latitude) or isset($longitude)){
     if($inline_data == 'EatAndDrinks'){
         $reply = 'Категории';
         takePos($token,$inline_chat_id,$dbconnect,$inline_user_id);
-        takePosName($token,$inline_user_id,$inline_chat_id,$dbconnect,$inline_data,);
+        //takePosName($token,$inline_user_id,$inline_chat_id,$dbconnect,$inline_data,);
         inlineKeyboard($token,$inline_chat_id,$reply,nextprev());
     }
     if($inline_data == 'backToCategory'){
