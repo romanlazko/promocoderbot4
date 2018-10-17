f8d9344b35662d9a877719fbb937a4a80af3ca80
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-17 18:57:08 +0300

    Update BD.php

--- BD.php
+++ BD.php
@@ -39,12 +39,21 @@ function takePos($token,$chat_id,$dbconnect,$user_id){
     //return $row['position'];
 }
 function takePosName($token,$user_id,$chat_id,$dbconnect,$inline_data){
-    $takePosName = "UPDATE `users` SET `posName` = '$inline_data' WHERE `user_id` = '$user_id'";
+    $takePosName = "UPDATE `users` SET `posName` = '$inline_data','position'='1' WHERE `user_id` = '$user_id'";
     if($dbconnect->query($takePosName) === TRUE){
         sendMessage($token,$chat_id,$user_id); 
     }
 }
-
+function position($token,$chat_id,$dbconnect,$user_id){
+    $result = $dbconnect->query("SELECT posName FROM EatAndDrinks WHERE posShow = '$position'");
+    while($row = $result->fetch_assoc()){
+        
+            sendMessage($token,$chat_id,$row['posName']);
+           
+        
+    }   
+    //return $row['position'];
+}
 // function create($token,$chat_id,$dbconnect){
 //     /*$login = "EatAndDrinks";
 //     $ucertable = "CREATE TABLE $login (
