acec0181ad1af4a7b825b1967eaf1911fe9c5c24
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-19 16:57:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -37,13 +37,13 @@ if(isset($latitude) or isset($longitude)){
 }
 if(isset($inline_data)){
     if($inline_data == 'EatAndDrinks'){
-        sendMessage($token,$inline_chat_id,'[​​​​​​​​​​​](https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Stack_Overflow_logo.svg/200px-Stack_Overflow_logo.svg.png) Some text here.');
-        $reply = 'Категория '.$inline_data;
+        //sendMessage($token,$inline_chat_id,'[​​​​​​​​​​​](https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Stack_Overflow_logo.svg/200px-Stack_Overflow_logo.svg.png) Some text here.');
+        $reply = '-';
         updateName($token,$inline_user_id,$inline_chat_id,$dbconnect,$inline_data,1);
         inlineKeyboard($token,$inline_chat_id,$reply,nextprev());
     }
     if($inline_data == 'nextfun'){
-        $reply = '-------------';
+        $reply = '-';
         $position = takeUserPos($dbconnect,$inline_user_id) + 1;
         updateName($token,$inline_user_id,$inline_chat_id,$dbconnect,takeUserName($dbconnect,$inline_user_id),$position);
         deleteMessage($token,$inline_chat_id,$message_id);
