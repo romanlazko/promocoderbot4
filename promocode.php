059470f78b9c496cf0df2f9bd8f9aab038f96b98
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-21 17:09:59 +0300

    Create promocode.php

---
+++ promocode.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+function promocode(){
+$chars="1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP"; 
+
+// Количество символов в пароле. 
+
+$max=10; 
+
+// Определяем количество символов в $chars 
+
+$size=StrLen($chars)-1; 
+
+// Определяем пустую переменную, в которую и будем записывать символы. 
+
+$password=null; 
+
+// Создаём пароль. 
+
+    while($max--) 
+    $password.=$chars[rand(0,$size)]; 
+
+} 
+?>
