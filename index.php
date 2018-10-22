5b0ed5ce568851db4f8728263974d34252c9cac8
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-22 19:47:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ if(isset($inline_data)){
         //sendMessage($token,$inline_chat_id,'[​​​​​​​​​​​](https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Stack_Overflow_logo.svg/200px-Stack_Overflow_logo.svg.png) Some text here.');
         $reply = 'Показать еще';
         
-        if(updateName($token,$user_id,$chat_id,$dbconnect,$inline_data,1) === TRUE){
+        if(updateName($user_id,$dbconnect,$inline_data,1) === TRUE){
             showPos($POS_NAME['position'],$token,$dbconnect,$chat_id);
         }
               
