05bafd29295c1970eb8c427fc230fd16d04b5844
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-22 23:27:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ if(isset($inline_data)){
             break;
         default:
             //if(setMore($inline_data,$dbconnect,takeUserData($dbconnect,$user_id)['posName']) === TRUE){
-            editMassage($token,$chat_id,$message_id,showMore($inline_data,$dbconnect),More($inline_data,1111));
+            editMassage($token,$chat_id,$message_id,showMore($inline_data,$dbconnect),More($inline_data,$inline_data));
             //}
     }
             
@@ -94,7 +94,7 @@ function nextprev(){
 }
 function More($more,$code){
     $more = array('text' => 'Подробнее', 'callback_data' => $more);
-    $promocode = array('text' => 'Получить промо-код', 'callback_data' => 'promocode');
+    $promocode = array('text' => 'Получить промо-код', 'callback_data' => 'promocode'.$code);
     $buttons = [
          [$more,$promocode]
     ];  
