5cbbb0fc5d451b4552a0005dcbc4c73e4df7a778
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-22 23:00:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ function nextprev(){
 }
 function More($more,$code){
     $more = array('text' => 'Подробнее', 'callback_data' => $more);
-    $promocode = array('text' => 'Получить промо-код', 'callback_data' => $code.'promocoder');
+    $promocode = array('text' => 'Получить промо-код', 'callback_data' => 'promocoder');
     $buttons = [
          [$more,$promocode]
     ];  
