a58d32270b13a8cef71534933c05716d2e24503a
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-23 23:50:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ if(isset($inline_data)){
     $category = substr($str, strrpos($str,"/")+1);
     $button = substr($str, 0, strrpos($str, '/'));
     $pos_id = substr($inline_data, strrpos($inline_data,"/")+1);
-    sendMessage($token,$chat_id,$str1.' '.$str2.' '.$str3);
+    sendMessage($token,$chat_id,$category.' '.$button.' '.$pos_id);
     switch ($inline_data) {
         case '/EatAndDrinks':        
             updateName($user_id,$dbconnect,$inline_data,1);
