def5fb2024b310ba44b368024947cac5712af8f6
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-23 17:08:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -39,9 +39,9 @@ if(isset($latitude) or isset($longitude)){
     inlineKeyboard($token,$chat_id,$reply,category());
 }
 if(isset($inline_data)){
-    //$str = substr($inline_data, 0, strrpos($inline_data, '/'));
-    $str1 = substr($inline_data, strrpos($inline_data,"/")-1);
-    //$str2 = substr($str1, 0, strrpos($str1, '/'));
+    $str = substr($inline_data, 0, strrpos($inline_data, '/'));
+    //$str1 = substr($inline_data, strrpos($inline_data,"/")+1);
+    $str2 = substr($str, 0, strrpos($str, '/'));
     sendMessage($token,$chat_id,$str.' '.$str1.' '.$str2);
     switch ($inline_data) {
         case 'EatAndDrinks':        
