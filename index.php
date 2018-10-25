173fe322cbfee680bdb7492e3366f2451cc13a60
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-26 01:42:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if(isset($inline_data)){
             $promocode = promocode();
                 promocodeExam($token,$chat_id,$dbconnect,$pos_id,$user_id,$promocode);
             $reply = posData($pos_id,$dbconnect,$category)['posName']."\n"."Промо-код: ".$promocode;
-            promocodeExam($token,$chat_id,$dbconnect,$pos_id,$user_id,promocode());
+            //promocodeExam($token,$chat_id,$dbconnect,$pos_id,$user_id,promocode());
             editMassage($token,$chat_id,$message_id,$reply,More($pos_id,$category,$pos_id));
             break;
         case 'nextfun':        
