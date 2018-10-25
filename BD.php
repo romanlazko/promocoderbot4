ef660bed2b36e0b6397d9f7366377289cbc77e64
romanlazko <43849612+romanlazko@users.noreply.github.com>
2018-10-26 01:13:49 +0300

    Update BD.php

--- BD.php
+++ BD.php
@@ -59,7 +59,7 @@ function promocodeExam($token,$chat_id,$dbconnect,$pos_id,$user_id,$promocode){
                                  FROM promocodes 
                                  WHERE pos_id = '$pos_id' AND user_id = '$user_id' LIMIT 1");
     while($row = $result->fetch_assoc()){        
-        if(isset($row[promocode])){
+        if(isset($row['promocode'])){
             sendMessage($token,$chat_id,'Промо-код есть');
         }else {
             $promocodeInsert = $dbconnect->query("INSERT INTO promocodes(pos_id,user_id,promocode) 
